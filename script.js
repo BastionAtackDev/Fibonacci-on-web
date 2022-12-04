1e01f365039ce3a9a2d11a8c65b585999c5be9a4
BastionAtackDev <88817320+BastionAtackDev@users.noreply.github.com>
2022-12-04 17:12:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 let i=0;
     let input = prompt('How many numbers of the Fibonacci sequence do you want to know ?');
+    let string = ""
     let a=1;
     let b=1;
     let c=0;
     while(i < input){
         c=a+b;
-        document.write(c+' ');
+        string = string + c+' ';
         b=c+a;
         i++;
         if(i<input){
-        document.write(b+' ');}
+        string = string + b+' ';}
         i++;
         a=b+c;
         if(i<input){
-        document.write(a+' ');}
+        string = string + a+' ';}
         i++;
     }
+document.write(string);
+document.getElementById('fibo').innerHTML = string;
